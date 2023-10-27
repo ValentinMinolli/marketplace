0803b4b664d4aab5b2c557e6002fb95024e8c088
Valentin Minolli <89503@sistemas.frc.utn.edu.ar>
2023-10-27 17:08:31 -0300

    Create User & Item API.

---
+++ marketplace/item/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path 
+
+from . import views
+
+app_name = 'item'
+
+urlpatterns =[
+    path('new/', views.new_item, name='new_item'),
+    path('<int:item_id>/', views.detail, name='detail'),
+]
